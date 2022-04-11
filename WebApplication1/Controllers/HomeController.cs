a8d08c2284c9dc13ab859f4986f02e237140db95
admin <admin@CHETUIWK015>
2022-04-11 15:42:45 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Db_context;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            coreContext DB = new coreContext();
+            var data = DB.EmpDtls.ToList();
+            DB.SaveChanges();
+            return View(data);
+
+          
+        }
+
+        public ActionResult Delete(int id)
+        {
+            coreContext DB = new coreContext();
+            var dateledata = DB.EmpDtls.Where(m => m.EmpId == id).First();
+            DB.EmpDtls.Remove(dateledata);
+            DB.SaveChanges();
+
+
+
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Db_Context/EmpDtl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApplication1.Db_context
+{
+    public partial class EmpDtl
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long? MbNo { get; set; }
+    }
+}
